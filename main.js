45e53162abbc5185d886d1d5ce1742aa03deb8ff
yukusunose <47027702+yukusunose@users.noreply.github.com>
2020-01-11 16:11:38 +0900

    add: test

--- main.js
+++ main.js
@@ -5,17 +5,22 @@
     var btn2 = document.getElementById('Featur__design__js__strings');
 
     btn.addEventListener('click', function(){
-          var n = Math.random()
+
+      var n = Math.random();
       if(n < 0.1){
         btn2.textContent = '大吉'; // 10%
       }else if (n < 0.3){
         btn2.textContent = '中吉';  // 20%
-      }else if (n < 0.6){
+　    }else if (n < 0.6){
         btn2.textContent = '小吉';  // 30%
-      }else if (n < 0.9){
+　    }else if (n < 0.9){
         btn2.textContent = '吉';  // 30%
-      }else{
+　    }else{
         btn2.textContent = '凶';  // 10%
-      }
+　    }
+      
     });
+
+
 })();
+
