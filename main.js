8bfba86b127c8c66370f9349e0d38ceeb26f8043
柴田豊 <shiutk@gmail.com>
2020-01-11 16:38:05 +0900

    結果取得APIの追加

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 (function () {
   'use strict';
 
@@ -6,16 +7,33 @@
 
   btn.addEventListener('click', function () {
     var n = Math.random()
+    var id;
     if (n < 0.1) {
       btn2.textContent = '大吉'; // 10%
+      id = '01';
     } else if (n < 0.3) {
       btn2.textContent = '中吉';  // 20%
+      id = '02';
     } else if (n < 0.6) {
       btn2.textContent = '小吉';  // 30%
+      id = '03';
     } else if (n < 0.9) {
       btn2.textContent = '吉';  // 30%
+      id = '04';
     } else {
       btn2.textContent = '凶';  // 10%
+      id = '05';
     }
+    // 結果をajaxで送る
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbzKoa3cnDSfHUz1KVfwc1f6ZQKYsXImMQ-mjh2srxTyKDnV-f4l/exec?code=' + id,
+      type: 'GET'
+    })
+    .done(function(data) {
+      console.log("正常");
+    })
+    .fail(function() {
+      console.log("エラー");
+    });
   });
 })();
